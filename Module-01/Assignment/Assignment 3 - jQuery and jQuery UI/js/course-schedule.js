2e33dbd387ee5ee25efd48b94085f0094174ff2c
Yoghantara <gyoghantara@gmail.com>
2022-04-20 22:46:31 +0800

    accordion

--- Module-01/Assignment/Assignment 3 - jQuery and jQuery UI/js/course-schedule.js
+++ Module-01/Assignment/Assignment 3 - jQuery and jQuery UI/js/course-schedule.js
@@ -1,22 +1,31 @@
 $(document).ready(function () {
   $("#header1").on("click", () => {
-    $("#schedule1").toggleClass("active");
+    $("#schedule1").addClass("active");
     $("#schedule2").removeClass("active");
     $("#schedule3").removeClass("active");
-    $(".arrow.a").toggleClass("active");
+
+    $(".arrow.a").addClass("active");
+    $(".arrow.b").removeClass("active");
+    $(".arrow.c").removeClass("active");
   });
 
   $("#header2").on("click", () => {
-    $("#schedule2").toggleClass("active");
+    $("#schedule2").addClass("active");
     $("#schedule1").removeClass("active");
     $("#schedule3").removeClass("active");
-    $(".arrow.b").toggleClass("active");
+
+    $(".arrow.b").addClass("active");
+    $(".arrow.a").removeClass("active");
+    $(".arrow.c").removeClass("active");
   });
 
   $("#header3").on("click", () => {
-    $("#schedule3").toggleClass("active");
+    $("#schedule3").addClass("active");
     $("#schedule1").removeClass("active");
     $("#schedule2").removeClass("active");
-    $(".arrow.c").toggleClass("active");
+
+    $(".arrow.c").addClass("active");
+    $(".arrow.a").removeClass("active");
+    $(".arrow.b").removeClass("active");
   });
 });
