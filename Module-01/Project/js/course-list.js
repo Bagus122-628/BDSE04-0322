5a189faf5194394266bd37a50042cd7a414438ef
Yoghantara <gyoghantara@gmail.com>
2022-04-29 16:14:30 +0800

    course list

--- Module-01/Project/js/course-list.js
+++ Module-01/Project/js/course-list.js
@@ -6,11 +6,14 @@ $(function () {
 });
 
 function frontEndList() {
+  document.querySelector(".header-CL h2").textContent =
+    "Front-End Web Developer";
   document.querySelector(".tab-frontend").style.display = "block";
   document.querySelector(".tab-backend").style.display = "none";
 }
 
 function backEndList() {
+  document.querySelector(".header-CL h2").textContent = "Back-End Developer";
   document.querySelector(".tab-frontend").style.display = "none";
   document.querySelector(".tab-backend").style.display = "block";
 }
