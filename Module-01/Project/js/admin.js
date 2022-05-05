d5e8167359fe6c68748d901fe69c38ef7a4e4adc
Yoghantara <gyoghantara@gmail.com>
2022-05-05 23:05:54 +0800

    generate data from local storage

--- Module-01/Project/js/admin.js
+++ Module-01/Project/js/admin.js
@@ -9,3 +9,20 @@ function getLocalStorageData() {
     json.push(JSON.parse(formData));
   }
 }
+
+let table = `<tr>`;
+json.map((value, index) => {
+  table += `<td>${index + 1}</td>
+            <td>${value.firstName} ${value.lastName}</td>
+            <td>${value.email}</td>
+            <td>${value.address}</td>
+            <td>${value.phone}</td>
+            <td>${value.selectCourse}</td>
+            <td>
+              <button>Edit</button>
+              <button>Delete</button>
+            </td>
+            </tr>`;
+});
+
+document.getElementById("data-table").insertAdjacentHTML("beforeend", table);
