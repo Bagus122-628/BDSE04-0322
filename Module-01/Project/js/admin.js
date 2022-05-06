4d4e337425029fc39c2e625ddb93940b539115e3
Yoghantara <gyoghantara@gmail.com>
2022-05-06 16:02:49 +0800

    feature: add data

--- Module-01/Project/js/admin.js
+++ Module-01/Project/js/admin.js
@@ -6,21 +6,77 @@ menuBar.addEventListener("click", () => {
   contentTable.classList.toggle("align-item-center");
 });
 
-let jsonFormData = JSON.parse(localStorage.getItem("formData"));
+if (localStorage.getItem("formData") != null) {
+  let jsonFormData = JSON.parse(localStorage.getItem("formData"));
 
-let table = `<tr>`;
-jsonFormData.map((value, index) => {
-  table += `<td style="text-align: center;">${index + 1}</td>
-            <td>${value.firstName} ${value.lastName}</td>
-            <td>${value.email}</td>
-            <td>${value.address}</td>
-            <td>${value.phone}</td>
-            <td>${value.selectCourse}</td>
-            <td>
-              <button class="btn btn-edit">Edit</button>
-              <button class="btn btn-delete">Delete</button>
-            </td>
-            </tr>`;
+  let table = `<tr>`;
+  jsonFormData.map((value, index) => {
+    table += `<td style="text-align: center;">${index + 1}</td>
+              <td>${value.firstName} ${value.lastName}</td>
+              <td>${value.email}</td>
+              <td>${value.address}</td>
+              <td>${value.phone}</td>
+              <td>${value.selectCourse}</td>
+              <td>
+                <button class="btn btn-edit">Edit</button>
+                <button class="btn btn-delete">Delete</button>
+              </td>
+              </tr>`;
+  });
+
+  document.getElementById("data-table").insertAdjacentHTML("beforeend", table);
+}
+
+// Add Data
+const overlayAddData = document.querySelector(".overlay-add-data");
+const formAddData = document.querySelector(".add-data");
+const btnAdd = document.querySelector(".btn-add");
+const btnCancel = document.querySelector(".btn-cancel");
+
+btnAdd.addEventListener("click", () => {
+  overlayAddData.classList.add("show");
 });
 
-document.getElementById("data-table").insertAdjacentHTML("beforeend", table);
+btnCancel.addEventListener("click", (event) => {
+  event.preventDefault();
+  overlayAddData.classList.remove("show");
+  formAddData.reset();
+});
+
+formAddData.addEventListener("submit", (event) => {
+  event.preventDefault();
+  handleLocalStorageData();
+  overlayAddData.classList.remove("show");
+  location.reload();
+});
+
+function handleLocalStorageData() {
+  if (localStorage.getItem("formData") === null) {
+    let formData = [];
+
+    formData.push({
+      firstName: $("#first-name").val(),
+      lastName: $("#last-name").val(),
+      email: $("#email").val(),
+      address: $("#address").val(),
+      phone: $("#phone").val(),
+      selectCourse: $("#select").val(),
+    });
+
+    localStorage.setItem("formData", JSON.stringify(formData));
+  } else {
+    let storedData = JSON.parse(localStorage.getItem("formData"));
+
+    const formData = {
+      firstName: $("#first-name").val(),
+      lastName: $("#last-name").val(),
+      email: $("#email").val(),
+      address: $("#address").val(),
+      phone: $("#phone").val(),
+      selectCourse: $("#select").val(),
+    };
+
+    storedData.push(formData);
+    localStorage.setItem("formData", JSON.stringify(storedData));
+  }
+}
