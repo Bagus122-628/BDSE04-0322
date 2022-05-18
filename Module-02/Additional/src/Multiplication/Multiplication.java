5d29c05111f818f9554026e3920c7a68eb5edb33
Yoghantara <gyoghantara@gmail.com>
2022-05-18 12:51:52 +0800

    multiplication

---
+++ Module-02/Additional/src/Multiplication/Multiplication.java
@@ -0,0 +1,15 @@
+public class Multiplication {
+  public static void main(String[] args) {
+    // 3.
+    System.out.println(-5 + 8 * 6); // 43
+    System.out.println((55 + 9) % 9); // 1
+    System.out.println(20 + (-3) * 5 / 8); // 19
+    System.out.println(5 + 15 / 3 * 2 - 8 % 3); // 13
+
+    // 4.
+    System.out.println("\n" + 4 * (1 - (1.0 / 3) + (1.0 / 5) - (1.0 / 7) + (1.0 / 9) - (1.0 / 11)));
+
+    // 5.
+    System.out.println("\n" + (25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+  }
+}
---
+++ Module-02/Additional/src/MultiplicationTable/Multiplication_table.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Multiplication_table {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Input number: ");
+    int userInput = input.nextInt();
+    multiplicationTable(userInput);
+  }
+
+  static void multiplicationTable(int userInput) {
+    for (int i = 1; i <= 10; i++) {
+      System.out.print(userInput + " * " + i + " = ");
+      System.out.println(userInput * i);
+    }
+  }
+}
