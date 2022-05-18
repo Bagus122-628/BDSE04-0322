6e764ab3731f8293e447b6200e7f95aaa561a1c3
Yoghantara <gyoghantara@gmail.com>
2022-05-18 12:52:22 +0800

    swap 2 numbers without using 3rd variables

---
+++ Module-02/Additional/src/SwapTwoNumbers/Swap_2_numbers.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Swap_2_numbers {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Input number 1: ");
+    int numb1 = input.nextInt();
+    System.out.print("Input number 2: ");
+    int numb2 = input.nextInt();
+    swapNumber(numb1, numb2);
+  }
+
+  static void swapNumber(int numb1, int numb2) {
+    System.out.println("Number 1 : " + numb1);
+    System.out.println("Number 2 : " + numb2);
+
+    numb1 = numb1 + numb2;
+    numb2 = numb1 - numb2;
+    numb1 = numb1 - numb2;
+
+    System.out.println("Swapped");
+    System.out.println("Number 1 : " + numb1);
+    System.out.println("Number 2 : " + numb2);
+  }
+}
