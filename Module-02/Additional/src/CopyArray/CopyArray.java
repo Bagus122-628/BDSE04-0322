15ec2c5f0a91f1778c6e1c819599591187f2846f
Yoghantara <gyoghantara@gmail.com>
2022-05-19 12:52:23 +0800

    copy of array

---
+++ Module-02/Additional/src/CopyArray/CopyArray.java
@@ -0,0 +1,19 @@
+package CopyArray;
+
+import java.util.Arrays;
+
+public class CopyArray {
+  public static void main(String[] args) {
+    int array1[] = new int[] { 1, 2, 3, 4, 5 };
+    int array2[] = new int[array1.length];
+
+    for (int i = 0; i < array1.length; i++) {
+      array2[i] = array1[i];
+    }
+
+    System.out.println("Array 1: " + Arrays.toString(array1));
+    System.out.println("Array 2: " + Arrays.toString(array2));
+    System.out.println(array1);
+    System.out.println(array2);
+  }
+}
