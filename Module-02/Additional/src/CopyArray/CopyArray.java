0c2972c675e9e8b9cc4ea9f210d5c9b4dcdf0438
Yoghantara <gyoghantara@gmail.com>
2022-05-19 21:10:48 +0800

    copy array

--- Module-02/Additional/src/CopyArray/CopyArray.java
+++ Module-02/Additional/src/CopyArray/CopyArray.java
@@ -11,9 +11,20 @@ public class CopyArray {
       array2[i] = array1[i];
     }
 
-    System.out.println("Array 1: " + Arrays.toString(array1));
-    System.out.println("Array 2: " + Arrays.toString(array2));
     System.out.println(array1);
     System.out.println(array2);
+
+    System.out.println("Array 1: ");
+    for (int i = 0; i < array1.length; i++) {
+      System.out.println(array1[i]);
+    }
+
+    System.out.println("Array 2: ");
+    for (int i = 0; i < array1.length; i++) {
+      System.out.println(array2[i]);
+    }
+
+    System.out.println("Array 1: " + Arrays.toString(array1));
+    System.out.println("Array 2: " + Arrays.toString(array2));
   }
 }
