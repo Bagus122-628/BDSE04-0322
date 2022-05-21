03d0566f006492e791f457aa1dd147f6a908e577
Yoghantara <gyoghantara@gmail.com>
2022-05-21 11:44:28 +0800

    employee

--- Module-02/Assignment/Assignment 2 - Company Structure/src/Employee.java
+++ Module-02/Assignment/Assignment 2 - Company Structure/src/Employee.java
@@ -1,3 +1,51 @@
 public class Employee {
+  private String name;
+  private double baseSalary;
+  private int employeeID;
+  private static int ID;
+  private Employee manager;
+  private String status;
 
+  public Employee(String name, double baseSalary) {
+    this.name = name;
+    this.baseSalary = baseSalary;
+    ID++;
+    this.employeeID = ID;
+  }
+
+  public double getBaseSalary() {
+    return baseSalary;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getEmployeeID() {
+    return employeeID;
+  }
+
+  public Employee getManager() {
+    return manager;
+  }
+
+  public void setManager(Employee manager) {
+    this.manager = manager;
+  }
+
+  public boolean equals(Employee other) {
+    return this.getEmployeeID() == other.getEmployeeID();
+  }
+
+  public String toString() {
+    return getEmployeeID() + " " + getName();
+  }
+
+  public String employeeStatus() {
+    return status;
+  }
+
+  public void setStatus(String status) {
+    this.status = status;
+  }
 }
