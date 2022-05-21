a9be350d1b49f168963f7f36dedbdd345ce972c4
Yoghantara <gyoghantara@gmail.com>
2022-05-21 11:48:39 +0800

    Technical Employee

--- Module-02/Assignment/Assignment 2 - Company Structure/src/TechnicalEmployee.java
+++ Module-02/Assignment/Assignment 2 - Company Structure/src/TechnicalEmployee.java
@@ -1,3 +1,18 @@
-public class TechnicalEmployee {
+public class TechnicalEmployee extends Employee {
+  private int checkIns;
+
+  public TechnicalEmployee(String name, double baseSalary) {
+    super(name, 75000);
+    checkIns = 0;
+  }
+
+  @Override
+  public String employeeStatus() {
+    return super.toString() + " has " + checkIns + " successful check ins";
+  }
+
+  public void setCheckin() {
+    checkIns++;
+  }
 
 }
