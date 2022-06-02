9adf461cc7e8750553ea7ca3a4755dbbdf0ead3b
Yoghantara <gyoghantara@gmail.com>
2022-06-02 16:49:40 +0800

    init A3

---
+++ Module-02/Assignment/Assignment 3 - Junit Testing/src/TechnicalEmployee.java
@@ -0,0 +1,18 @@
+public class TechnicalEmployee extends Employee {
+  public int checkIns;
+
+  public TechnicalEmployee(String name) {
+    super(name, 75000.00);
+    checkIns = 0;
+  }
+
+  @Override
+  public String employeeStatus() {
+    return super.toString() + " has " + checkIns + " successful check ins";
+  }
+
+  public void setCheckin() {
+    checkIns++;
+  }
+
+}
