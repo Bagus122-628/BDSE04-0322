c5617dd7428af594727dc3581187b9cb8ff2bea8
Yoghantara <gyoghantara@gmail.com>
2022-06-09 16:48:55 +0800

    A3

--- Module-02/Assignment/Assignment 3 - Junit Testing/test/SoftwareEngineerTest.java
+++ Module-02/Assignment/Assignment 3 - Junit Testing/test/SoftwareEngineerTest.java
@@ -1,7 +1,7 @@
-import static org.junit.Assert.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class SoftwareEngineerTest {
 
