abd9ec8f0d172a59047500ff24cf28c18c93fe25
Yoghantara <gyoghantara@gmail.com>
2022-07-06 14:41:13 +0800

    card detail component

--- Module-03/Assignment/Assignment 4 - Angular/car-dealership/src/app/app-routing.module.ts
+++ Module-03/Assignment/Assignment 4 - Angular/car-dealership/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarDetailComponent } from './car-detail/car-detail.component';
 import { CarsListComponent } from './cars-list/cars-list.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 const routes: Routes = [
   { path: '', component: WelcomeComponent },
   { path: 'list-of-cars', component: CarsListComponent },
+  { path: 'car-detail', component: CarDetailComponent },
 ];
 
 @NgModule({
--- Module-03/Assignment/Assignment 4 - Angular/car-dealership/src/app/app.module.ts
+++ Module-03/Assignment/Assignment 4 - Angular/car-dealership/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CarsListComponent } from './cars-list/cars-list.component';
+import { CarDetailComponent } from './car-detail/car-detail.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { CarsListComponent } from './cars-list/cars-list.component';
     WelcomeComponent,
     NavbarComponent,
     CarsListComponent,
+    CarDetailComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
