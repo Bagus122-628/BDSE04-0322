1d1c4f702dc01a2b73f4e748cb443ca67ed40d3f
Yoghantara <gyoghantara@gmail.com>
2022-07-09 11:21:03 +0800

    featured jobs

--- Module-03/Project/abc-jobs/src/app/pages/home/home.component.ts
+++ Module-03/Project/abc-jobs/src/app/pages/home/home.component.ts
@@ -1,5 +1,64 @@
 import { Component, OnInit } from '@angular/core';
-import { PARTNERS } from './partners';
+
+const PARTNERS = [
+  {
+    logo: '../../../assets/images/Partners/aws.png',
+    alt: 'AWS',
+  },
+  {
+    logo: '../../../assets/images/Partners/facebook.png',
+    alt: 'Facebook',
+  },
+  {
+    logo: '../../../assets/images/Partners/google.png',
+    alt: 'Google',
+  },
+  {
+    logo: '../../../assets/images/Partners/lithan.png',
+    alt: 'Lithan',
+  },
+  {
+    logo: '../../../assets/images/Partners/microsoft.png',
+    alt: 'Microsoft',
+  },
+  {
+    logo: '../../../assets/images/Partners/samsung.png',
+    alt: 'Samsung',
+  },
+];
+
+const TOPICS = [
+  { topic: 'Remote' },
+  { topic: 'Internship' },
+  { topic: 'Freelancer' },
+  { topic: 'Edutation' },
+  { topic: 'Full Time' },
+  { topic: 'Design' },
+];
+
+const FEATUREDJOBS = [
+  {
+    logo: '../../../assets/images/logo/google.png',
+    alt: 'Google',
+    jobName: 'Web Developer',
+    company: 'Google.com',
+    tags: ['Internship', 'Remote'],
+  },
+  {
+    logo: '../../../assets/images/logo/microsoft.png',
+    alt: 'Microsoft',
+    jobName: 'App Developer',
+    company: 'Microsoft.com',
+    tags: ['Full Time', 'Senior'],
+  },
+  {
+    logo: '../../../assets/images/logo/facebook.png',
+    alt: 'Facebook',
+    jobName: 'Web Developer',
+    company: 'Facebook.com',
+    tags: ['Internship', 'Full Time'],
+  },
+];
 
 @Component({
   selector: 'app-home',
@@ -8,6 +67,9 @@ import { PARTNERS } from './partners';
 })
 export class HomeComponent implements OnInit {
   partners = PARTNERS;
+  topics = TOPICS;
+  jobs = FEATUREDJOBS;
+
   constructor() {}
 
   ngOnInit(): void {}
