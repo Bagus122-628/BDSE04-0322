ea863c21071aa6ab458ae07b12925c69a1b6fbbf
Yoghantara <gyoghantara@gmail.com>
2022-07-09 15:47:47 +0800

    registration confirmation

--- Module-03/Project/abc-jobs/src/app/app-routing.module.ts
+++ Module-03/Project/abc-jobs/src/app/app-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './users/login/login.component';
+import { RegistrationConfirmationComponent } from './users/registration/registration-confirmation/registration-confirmation.component';
 import { RegistrationComponent } from './users/registration/registration.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'registration', component: RegistrationComponent },
+  {
+    path: 'registration-confirmation',
+    component: RegistrationConfirmationComponent,
+  },
 ];
 
 @NgModule({
--- Module-03/Project/abc-jobs/src/app/app.module.ts
+++ Module-03/Project/abc-jobs/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { LoginFormComponent } from './users/login-form/login-form.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { LoginComponent } from './users/login/login.component';
 import { RegistrationComponent } from './users/registration/registration.component';
+import { RegistrationConfirmationComponent } from './users/registration/registration-confirmation/registration-confirmation.component';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { RegistrationComponent } from './users/registration/registration.compone
     FooterComponent,
     LoginComponent,
     RegistrationComponent,
+    RegistrationConfirmationComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
