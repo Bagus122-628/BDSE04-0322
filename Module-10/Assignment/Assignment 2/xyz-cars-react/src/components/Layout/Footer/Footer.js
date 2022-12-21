07c1371c0770c0f98bd0c5d4fa6c6dcb5e2933d7
Yoghantara <gyoghantara@gmail.com>
2022-12-21 13:27:20 +0800

    M10 A2

---
+++ Module-10/Assignment/Assignment 2/xyz-cars-react/src/components/Layout/Footer/Footer.js
@@ -0,0 +1,13 @@
+const Footer = (props) => {
+  return (
+    <footer className={props.className}>
+      <div>
+        <p className="text-secondary text-center">
+          Created by Gustut Yoghantara
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
